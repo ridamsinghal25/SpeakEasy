67819cb08a00030b3119891375bebec4205a8770
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-01-11 15:38:47 +0530

    add upload to cloudinary method

---
+++ src/utils/uploadToCloudinary.js
@@ -0,0 +1,24 @@
+// lib/cloudinary.js
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadToCloudinary = (fileBuffer, resourceType) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: resourceType },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+    uploadStream.end(fileBuffer);
+  });
+};
